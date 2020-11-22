8d8d4d3fdd1079e5f3fb043656b2fcf0f9074b8a
Md-Asif-Mahmud789 <74695984+Md-Asif-Mahmud789@users.noreply.github.com>
2020-11-23 03:52:09 +0600

    Add files via upload

---
+++ simple_static_methode/src/static_Methode/Static.java
@@ -0,0 +1,24 @@
+ 
+package firstproject;
+ 
+public class Static {
+    
+    public static void main(String[] args) {
+        
+        //static class er object..... 
+        StaticClass obj1=new StaticClass();
+        obj1.display();
+        
+        
+        StaticClass.display1();
+        
+    
+        
+    }
+    
+    
+    
+    
+    
+    
+}
---
+++ simple_static_methode/src/static_Methode/StaticClass.java
@@ -0,0 +1,23 @@
+ 
+package firstproject;
+ 
+public class StaticClass {
+    
+    void display(){ 
+        System.out.println("this is not static  ");
+     
+    }
+    
+    static void display1(){
+        
+        System.out.println("this is static ");
+      
+        
+    }
+    
+    
+    
+    
+    
+    
+}
